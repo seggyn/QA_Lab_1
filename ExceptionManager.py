610a7897dd35d342619fac7231e6f815fdc82557
Andrii <anrey.pk@gmail.com>
2019-10-02 18:12:46 +0300

    Lab 2

--- ExceptionManager.py
+++ ExceptionManager.py
@@ -1,16 +1,39 @@
+from Stubs.ExceptionTypeValidatorFactory import ExceptionTypeValidatorFactory
+import configparser
+
+
 class ExceptionManager:
 
-    CritiicalExceptionsArray = ['ErrorConnectionReset', 'ErrorInternetDisconnected', 'MemoryError', 'OSError']
+    ServerSendingErrorCounter = 0
+    CriticalExceptionCounter = 0
+    NotCriticalExceptionCounter = 0
+    Verifier = None
 
-    def __init__(self):
-        self.CriticalExceptionCounter = 0
-        self.NotCriticalExceptionCounter = 0
+    def __init__(self, sender):
+        self.Sender = sender
+        self.Validator = ExceptionTypeValidatorFactory.create()
+        self.CriticalExceptionsArray = self.getExceptionsFromConfig()
+
+    def getExceptionsFromConfig(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        string = config['EXCEPTIONS']['critical']
+        return string.replace(' ', '').split(',')
+
+    def isExceptionType(self, exception):
+        return self.Validator.isExceptionType(exception)
 
     def isCriticalException(self, exception):
-        return True if exception.__name__ in self.CritiicalExceptionsArray else False
+        return self.Verifier.isCriticalException(exception, self.CriticalExceptionsArray)
+
+    def sendExceptionToServer(self, exception):
+        self.Sender.sendExceptionToServer(exception)
 
     def handle(self, exception):
-        if self.isCriticalException(exception):
-            self.CriticalExceptionCounter += 1
-        else:
-            self.NotCriticalExceptionCounter += 1
+        if self.isExceptionType(exception):
+            if self.isCriticalException(exception):
+                self.CriticalExceptionCounter += 1
+                if not self.sendExceptionToServer(exception):
+                    self.ServerSendingErrorCounter += 1
+            else:
+                self.NotCriticalExceptionCounter += 1
--- TestExceptionManager.py
+++ TestExceptionManager.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 
 from ExceptionManager import ExceptionManager
 
@@ -8,11 +9,24 @@ from Exceptions.ErrorConnectionReset import ErrorConnectionReset
 from Exceptions.ErrorCacheMiss import ErrorCacheMiss
 from Exceptions.ErrorSSLProtocolError import ErrorSSLProtocolError
 
+from Stubs.Sender import Sender
+from Stubs.CriticalVerifier import CriticalVerifier
 
 class TestExceptionManager(unittest.TestCase):
 
     def setUp(self):
-        self.manager = ExceptionManager()
+        self.manager = ExceptionManager(Sender())
+        self.manager.Verifier = CriticalVerifier()
+
+    def test_is_exception_type_exception(self):
+        self.assertTrue(self.manager.isExceptionType(ErrorInternetDisconnected))
+        self.assertTrue(self.manager.isExceptionType(ErrorConnectionReset))
+        self.assertTrue(self.manager.isExceptionType(MemoryError))
+        self.assertTrue(self.manager.isExceptionType(OSError))
+
+    def test_is_exception_type_not_exception(self):
+        self.assertFalse(self.manager.isExceptionType(Sender))
+        self.assertFalse(self.manager.isExceptionType(CriticalVerifier))
 
     def test_is_critical_exception_critical(self):
         self.assertTrue(self.manager.isCriticalException(ErrorInternetDisconnected))
@@ -25,15 +39,24 @@ class TestExceptionManager(unittest.TestCase):
         self.assertFalse(self.manager.isCriticalException(ErrorCacheMiss))
         self.assertFalse(self.manager.isCriticalException(ImportError))
 
-    def test_handle_zero(self):
-        self.assertTrue(self.manager.CriticalExceptionCounter == 0)
-        self.assertTrue(self.manager.NotCriticalExceptionCounter == 0)
-
-    def test_handle_no_zero(self):
+    def test_handle_not_zero(self):
         self.manager.handle(ErrorInternetDisconnected)
         self.manager.handle(ErrorConnectionReset)
         self.manager.handle(ErrorCacheMiss)
 
         self.assertTrue(self.manager.CriticalExceptionCounter == 2)
         self.assertTrue(self.manager.NotCriticalExceptionCounter == 1)
+
+    def test_sender(self):
+        mock = Mock()
+        mock.sendExceptionToServer.return_value = False
+
+        self.manager = ExceptionManager(mock)
+        self.manager.Verifier = CriticalVerifier()
+
+        self.manager.handle(ErrorInternetDisconnected)
+        self.manager.handle(ErrorConnectionReset)
+
+        self.assertTrue(self.manager.ServerSendingErrorCounter == 2)
+
 
